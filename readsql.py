df1b71340a7ba6fe601734fffd6bb3209863000f
ridhima <ridhimasharma.1723@gmail.com>
2024-05-30 12:15:44 +0530

    Module readsql created.analysis

---
+++ readsql.py
@@ -0,0 +1,70 @@
+import os 
+
+import pandas as pd 
+import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
+
+load_dotenv()
+host = os.getenv('HOST')
+username = os.getenv('USER')
+password = os.getenv('PASSWORD')
+
+connection = mysql.connector.connect(host='localhost',
+                                      user='root',
+                                      password='1723',
+                                      database='swiftmarket')
+
+cursor = connection.cursor()
+
+def queryToDataFrame (query):
+
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    df = pd.DataFrame(data=rows,columns=cursor.column_names)
+    return df
+
+def showTables():
+    query = """SHOW TABLES;"""
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    df = dfDataFrame(data=rows,columns=cursor.column_names)
+    return df 
+
+def describeTable(tablename):
+    query = f"""describe{tablename};"""
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    df = pd.DataFrame(data=rows,columns=cursor.column_names)
+    return df 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
